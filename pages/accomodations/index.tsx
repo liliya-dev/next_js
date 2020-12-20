d2949aafa97acd3973df590fbe823758341a3b21
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2020-12-20 17:11:28 +0200

    build

--- pages/accomodations/index.tsx
+++ pages/accomodations/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import classes from './AccomodationsPage.module.scss';
+import classes from './accomodationsPage.module.scss';
 import { NextPage } from "next";
 import { useRouter } from "next/router";
 import { AccomodationsList } from "../../components/AccomodationsList/AccomodationsList";
