15fb176007161f442427aeab17db1dcde5ae315d
lili4ka <lili4ka@MacBook-Pro-lili4ka.local>
2020-12-21 19:02:46 +0200

    added home button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import classes from './searchPage.module.scss';
+import classes from './search/searchPage.module.scss';
 import { NextPage } from 'next';
 import { MainLayout } from '../components/MainLayout/MainLayout';
 import { SearchForm } from '../components/SearchComponents/SearchForm/SearchForm';
